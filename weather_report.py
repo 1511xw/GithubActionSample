15f37e0b106831bc6f40e633c9878a777dd3645c
1511xw <49946718+1511xw@users.noreply.github.com>
2024-01-07 15:49:43 +0800

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -92,7 +92,7 @@ def send_weather(access_token, weather):
     body = {
         "touser": openId.strip(),
         "template_id": weather_template_id.strip(),
-        "url": "https://weixin.qq.com",
+        "url": "https://www.baidu.com",
         "data": {
             "date": {
                 "value": today_str
